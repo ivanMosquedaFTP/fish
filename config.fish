cff65b0ffb48ac378b4842beba5cf7577dac1f67
ivanMosquedaFTP <124939131+ivanMosquedaFTP@users.noreply.github.com>
2024-06-26 18:03:43 -0600

    configuration stage1

---
+++ config.fish
@@ -0,0 +1,50 @@
+# Put system-wide fish configuration entries here
+# or in .fish files in conf.d/
+# Files in conf.d can be overridden by the user
+# by files with the same name in $XDG_CONFIG_HOME/fish/conf.d
+
+# This file is run by all fish instances.
+# To include configuration only for login shells, use
+# if status is-login
+#    ...
+# end
+# To include configuration only for interactive shells, use
+if status is-interactive
+	set fish_greeting ""
+	neofetch
+end
+
+#general purpose aliases
+alias off='sudo shutdown now'
+alias cdp='cd /home/toreve/projects'
+alias cdj='cd /home/toreve/projects/java'
+alias cdc='cd /home/toreve/projects/c'
+alias cdpp='cd /home/toreve/projects/python'
+alias cdw='cd /home/toreve/projects/web'
+
+#package manager aliases
+alias update='sudo pacman -Syy'
+alias yupdate='yay -Syy'
+alias upgrade='sudo pacman -Syyu'
+alias yupgrade='yay -Syyu'
+alias install='sudo pacman -S'
+alias yinstall='yay -S'
+alias search='sudo pacman -Ss'
+alias ysearch='yay -Ss'
+alias query='sudo pacman -Q'
+alias yquery='yay -Q'
+alias remove='sudo pacman -R'
+alias yremove='yay -R'
+
+#services aliases
+alias statusService='sudo systemctl status'
+alias startService='sudo systemctl start'
+alias restartService='sudo systemctl restart'
+alias stopService='sudo systemctl stop'
+alias enableService='sudo systemctl enable'
+alias disableService='sudo systemctl disable'
+
+#configuration files aliases
+alias fishc='sudo vim /etc/fish/config.fish'
+alias neo='cd ~/.config/nvim/ && nvim init.lua'
+alias hypr='cd ~/.config/hypr/ && nvim'
